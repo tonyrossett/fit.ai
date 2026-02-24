6c8c3301ae7e6b889026fb3e59feabecc92392d0
tonyrossett <tonyrossett@hotmail.com>
2026-02-24 14:24:42 -0300

    feat: add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World!!");
+import 'dotenv/config';
+import Fastify from 'fastify';
+const fastify = Fastify({
+  logger: true,
+})
+
+// Declare a route
+fastify.get('/', async function handler () {
+  return { hello: 'world' };
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: Number(process.env.PORT) || 8081 });
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
